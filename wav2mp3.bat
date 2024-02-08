d4ab9d8485e96d47e069d8b48c588ea2c659d3e3
wtfloltk <153698146+wtfloltk@users.noreply.github.com>
2024-02-08 14:27:35 -0500

    Update wav2mp3.bat

---
+++ wav2mp3.bat
@@ -0,0 +1,1 @@
+ffmpeg.exe -i - -af pan="7.1|c0=c21+c20+c19+c0+c1|c1=c18+c17+c16+c15+c14+c2|c2=c26+c25+c24+c23+c22+c3+c4+c5+c6+c7|c3=c8+c9+c10+c11|c4=c12+c13+c14|c5=c16+c17+c18+c19+c20|c6=c21+c22+c20+c27+c28+c29|c7=c0+c1+c2+c3+c4+c5+c6+c7+c8+c9+c10+c19+c18+c31+c30" out.mp3
